6a364a880e3db73b2e50836198e66b6c3be55c2d
Brune <abrune.osteo@gmail.com>
2023-02-17 13:05:45 +0100

    jsx update

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Menu from "./components/Menu"
+import Favorite from "./components/Favorite"
+
+function App() {
+    return (
+        <div>
+            <Menu />
+            <hr />
+            <Favorite />
+        </div>
+    )
+}
+
+export default App
